d8af8baeeef99c7cf54a77950735b8bf0ca6c875
ganwa-laure <anne123laure321@gmail.com>
2026-01-15 20:52:48 +0200

    Add TypeScript folder

---
+++ TypeScript/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(0);
+
+if (customer !== null && customer !== undefined)
+    
+    console.log(customer.birthday);
+//# sourceMappingURL=index.js.map
